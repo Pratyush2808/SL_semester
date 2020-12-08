d085edb9ed7277039565d390f2bb501c91d76976
unknown <yusufcricket200@gmail.com>
2020-12-08 08:53:04 +0530

    after routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedbackCreateComponent } from './feedback/feedback-create/feedback-create.component';
+import { FeedbackListComponent } from './feedback/feedback-list/feedback-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FeedbackCreateComponent },
+  { path: 'list', component: FeedbackListComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const appRoutingModule = RouterModule.forRoot(routes);
